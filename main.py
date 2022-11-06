d903a072559f409837fef76a070fd44090db16fe
Michael Brink <michaelbrink3@gmail.com>
2022-11-05 22:35:18 -0700

    create a scheduler

--- main.py
+++ main.py
@@ -2,23 +2,15 @@ from update_helpers import *
 import threading
 from threading import Thread
 import time
+import schedule
+
 
 '''
 Using threads for async function execution
 '''
-Thread(target = run_score_update).start()
 
+schedule.every(1).minutes.do(run_score_update)
 
-
-# def run():
-#     threading.Timer(15.0, run_score_update).start()
-#     print ("Hello, World!")
-
-#run_score_update()
-    
-#Thread(target = run_score_update).start()
-#Thread(target = run_leaderboard, args=("0x691C7c07A1B1698c56340d386d8cC7A823f6e2D8",)).start()
-
-
-#0x9017804aE02877C32739A7703400326e9Ac9a04d
-
+while 1:
+    schedule.run_pending()
+    time.sleep(1)
